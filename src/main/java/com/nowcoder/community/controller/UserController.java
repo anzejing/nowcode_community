249b31de419ccf180807d5e3a2b5545c109143f0
anzejing <870872819@qq.com>
2024-03-08 20:14:58 +0800

    登录模块

---
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -0,0 +1,134 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.annotation.LoginRequired;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${Community.path.upload}")
+    private String uploadPath;
+
+    @Value("${Community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+    @LoginRequired
+    @RequestMapping(path = "/setting",method = RequestMethod.GET)
+    public String getSettingPage(){
+        return "/site/setting";
+    }
+    @LoginRequired
+    @RequestMapping(path = "/upload",method = RequestMethod.POST)
+    public String uploadHeader(MultipartFile headerImage, Model model){
+        if(headerImage==null){
+            model.addAttribute("error","您还没有选择图片！");
+            return "/site/setting";
+        }
+
+        String fileName = headerImage.getOriginalFilename();
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        if(StringUtils.isBlank(suffix)){
+            model.addAttribute("error","文件格式不正确！");
+            return "/site/setting";
+        }
+
+        //生成随机文件名
+        fileName = CommunityUtil.generateUUID()+suffix;
+        //确定文件位置
+        File dest = new File(uploadPath+"/"+fileName);
+        try {
+            headerImage.transferTo(dest);
+        } catch (IOException e) {
+            logger.error("上传文件失败"+e.getMessage());
+            throw new RuntimeException("上传文件失败,服务器发送异常",e);
+        }
+
+        //更新用户头像路径(WEB访问路径）
+        User user = hostHolder.getUser();
+        String headerUrl = domain+contextPath+"/user/header/"+fileName;
+        userService.updateHeader(user.getId(),headerUrl);
+
+        return "redirect:/index";
+
+    }
+
+    @RequestMapping(path = "/header/{filename}",method =RequestMethod.GET)
+    public  void getHeader(@PathVariable("filename") String fileName, HttpServletResponse response){
+        //服务器存放位置
+        fileName = uploadPath+"/"+fileName;
+        //文件后缀
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        //响应图片
+        response.setContentType("image/"+suffix);
+        try (
+                FileInputStream fis =new FileInputStream(fileName);
+                OutputStream os = response.getOutputStream();
+        ){
+            byte[] buffer =new  byte[1024];
+            int b =0;
+            while ((b = fis.read(buffer))!=-1){
+                os.write(buffer,0,b);
+            }
+        } catch (IOException e) {
+            logger.error("读取头像失败"+e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+    @LoginRequired
+    @RequestMapping(path = "/changePassword",method = RequestMethod.POST)
+    public String changePassword(String oldPassword,String newPassword,Model model){
+        if(StringUtils.isBlank(oldPassword)){
+            model.addAttribute("oldPasswordError","请输入原始密码！");
+            return "/site/setting";
+        }
+        User user = hostHolder.getUser();
+        oldPassword = CommunityUtil.md5(oldPassword+user.getSalt());
+        if(!user.getPassword().equals(oldPassword)){
+            model.addAttribute("oldPasswordError","密码不正确！");
+            return "/site/setting";
+        }
+        if(StringUtils.isBlank(newPassword)){
+            model.addAttribute("newPasswordError","请输入新密码！");
+            return "/site/setting";
+        }
+        newPassword = CommunityUtil.md5(newPassword+user.getSalt());
+        userService.changePassword(user.getId(),newPassword);
+        return "redirect:/index";
+    }
+
+
+}
--- src/main/java/com/nowcoder/community/util/CommunityUtil.java
+++ src/main/java/com/nowcoder/community/util/CommunityUtil.java
@@ -8,12 +8,16 @@ import java.util.UUID;
 
 public class CommunityUtil {
 
-    //生成随机字符串
+    /**
+     * 生成随机字符串
+     */
     public static String generateUUID(){
         return UUID.randomUUID().toString().replaceAll("-","");
     }
 
-    //MD5加密
+    /**
+     * MD5加密
+     */
     public static String md5(String key){
         if(StringUtils.isBlank((key))){
             return null;
